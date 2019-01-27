1b6d04c2b3bcdd97133da5b48dd48fded5f94a0b
Dametislevu <adam@dametislevu.cz>
2019-01-27 18:37:53 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,31 +3,29 @@ window.onload = function() {
   const navigation = document.getElementById('navigation');
   const menu = document.getElementById('menu');
 
+  function mobileMenuToggle(){
+    burgerMenuIcon.classList.toggle('burger-menu-icon--slide-in');
+    navigation.classList.toggle('navigation--slide-in');
+    menu.classList.toggle('menu--slide-in');
+  }
+
   if (window.innerWidth <= 1366){
     navigation.addEventListener("click", mobileMenu, false);
     function mobileMenu(e){
       if (menu.classList.contains('menu--slide-in')){
         if (e.target.nodeName == "A"){
-          burgerMenuIcon.classList.remove('burger-menu-icon--slide-in');
-          navigation.classList.remove('navigation--slide-in');
-          menu.classList.remove('menu--slide-in');
+          mobileMenuToggle();
         }
         else if (e.target.classList.contains('burger-menu-icon')){
-          burgerMenuIcon.classList.remove('burger-menu-icon--slide-in');
-          navigation.classList.remove('navigation--slide-in');
-          menu.classList.remove('menu--slide-in');
+          mobileMenuToggle();
         }
         else if (e.target.classList.contains('burger-menu-icon__line')){
-          burgerMenuIcon.classList.remove('burger-menu-icon--slide-in');
-          navigation.classList.remove('navigation--slide-in');
-          menu.classList.remove('menu--slide-in');
+          mobileMenuToggle();
         }
       }
       else {
-        burgerMenuIcon.classList.add('burger-menu-icon--slide-in');
-        navigation.classList.add('navigation--slide-in');
-        menu.classList.add('menu--slide-in');
+        mobileMenuToggle();
       }
-    }   
+    }
   }
 }
