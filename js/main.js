31510d4e70ebc7af8a0b8bba6499cb955889123a
Dametislevu <adam@dametislevu.cz>
2019-01-27 20:51:28 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -2,11 +2,15 @@ window.onload = function() {
   const burgerMenuIcon = document.getElementById('burger-menu-icon');
   const navigation = document.getElementById('navigation');
   const menu = document.getElementById('menu');
+  const burgerMenuIconItems = burgerMenuIcon.querySelectorAll('.burger-menu-icon__line');
 
   function mobileMenuToggle(){
     burgerMenuIcon.classList.toggle('burger-menu-icon--slide-in');
     navigation.classList.toggle('navigation--slide-in');
     menu.classList.toggle('menu--slide-in');
+    for (var i = 0; i < burgerMenuIconItems.length; i++) {
+      burgerMenuIconItems[i].classList.toggle('burger-menu-icon__line--slide-in');
+    }
   }
 
   if (window.innerWidth <= 1366){
